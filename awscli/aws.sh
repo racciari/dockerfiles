7e17fc74e628b77059ce056a3bf908bd3dc96b74
Romain Acciari <romain.acciari@gmail.com>
2017-08-18 07:56:47 +0200

    awscli: Add region parameter

--- awscli/aws.sh
+++ awscli/aws.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Defaults
+: ${REGION:=us-east-1}
+
 if [ -z "$AWS_ACCESS_KEY_ID" -a -z "$AWS_SECRET_ACCESS_KEY" ]; then
   echo 'AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are not specified.'
   exit 1
@@ -8,5 +11,10 @@ fi
 sed -i -e "s@AWS_ACCESS_KEY_ID@$AWS_ACCESS_KEY_ID@g" \
        -e "s@AWS_SECRET_ACCESS_KEY@$AWS_SECRET_ACCESS_KEY@g" \
   ~/.aws/credentials
+
+if [ ! -z "$REGION" ]; then
+  echo "region = $REGION" \
+    >>~/.aws/config
+fi
 
 /usr/bin/aws "$@"
