9dba8b51f5ebc4073312aa3208790ed6c4ebd6f9
Romain Acciari <romain.acciari@gmail.com>
2017-08-18 08:42:55 +0200

    awscli: Add signature option

--- awscli/aws.sh
+++ awscli/aws.sh
@@ -8,12 +8,20 @@ if [ -z "$AWS_ACCESS_KEY_ID" -a -z "$AWS_SECRET_ACCESS_KEY" ]; then
   exit 1
 fi
 
+# Set credentials
 sed -i -e "s@AWS_ACCESS_KEY_ID@$AWS_ACCESS_KEY_ID@g" \
        -e "s@AWS_SECRET_ACCESS_KEY@$AWS_SECRET_ACCESS_KEY@g" \
   ~/.aws/credentials
 
+# Set config
 if [ ! -z "$REGION" ]; then
   echo "region = $REGION" \
+    >>~/.aws/config
+fi
+if [ ! -z "$SIGNATURE" ]; then
+  echo "s3 =" \
+    >>~/.aws/config
+  echo "    signature_version = $SIGNATURE" \
     >>~/.aws/config
 fi
 
