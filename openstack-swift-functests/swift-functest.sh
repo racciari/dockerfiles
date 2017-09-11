8938344c7fc5623d5e388a27d2384a2ce380996d
Romain Acciari <romain.acciari@gmail.com>
2017-09-11 14:34:58 +0200

    openstack-swift-functests: Update to Openstack Pike

--- openstack-swift-functests/swift-functest.sh
+++ openstack-swift-functests/swift-functest.sh
@@ -8,6 +8,14 @@ if [ ! -z "$SWIFT_PORT" ]; then
   sed -i -e "s@^auth_port =.*@auth_port = ${SWIFT_PORT}@" /etc/swift/test.conf
 fi
 
+if [ ! -z "$AUTH_PREFIX" ]; then
+  sed -i -e "s@^auth_prefix =.*@auth_prefix = ${AUTH_PREFIX}@" /etc/swift/test.conf
+fi
+
+if [ ! -z "$AUTH_VERSION" ]; then
+  sed -i -e "s@^#auth_version =.*@auth_version = ${AUTH_VERSION}@" /etc/swift/test.conf
+fi
+
 if [ ! -z "$SWIFT_SSL" ]; then
   sed -i -e "s@^auth_ssl =.*@auth_ssl = ${SWIFT_SSL}@" /etc/swift/test.conf
 fi
